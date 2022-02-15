780de4f1267fa4f1b240b73754c5234df8954567
trainraider <trainraider7@gmail.com>
2022-02-15 01:08:29 -0500

    Hello World

---
+++ src/main/java/com/gmail/trainraider7/spawncontrol/SpawnControl.kt
@@ -0,0 +1,15 @@
+package com.gmail.trainraider7.spawncontrol
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class SpawnControl : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        println("Hello world")
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
